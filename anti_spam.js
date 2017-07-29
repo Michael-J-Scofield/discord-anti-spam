128359f159c1a78c8923f174dfe7f328d9ad87bb
Michael <michaelscofield4554@gmail.com>
2017-07-29 21:28:13 +0200

    Added duplicate message spam detection

--- anti_spam.js
+++ anti_spam.js
@@ -1,6 +1,7 @@
 const authors = [];
 var warned = [];
 var banned = [];
+var messagelog = [];
 
 /**
  * Add simple spam protection to your discord server.
@@ -15,47 +16,99 @@ module.exports = function (bot, options) {
   const interval = (options && options.interval) || 1000;
   const warningMessage = (options && options.warningMessage) || "stop spamming or I'll whack your head off.";
   const banMessage = (options && options.banMessage) || "has been banned for spamming, anyone else?";
+  const maxDuplicatesWarning = (options && options.duplicates || 7);
+  const maxDuplicatesBan = (options && options.duplicates || 10);
 
   bot.on('message', msg => {
+
     if(msg.author.id != bot.user.id){
       var now = Math.floor(Date.now());
       authors.push({
         "time": now,
-        "author": msg.author.id,
-        "message": msg.content
+        "author": msg.author.id
+      });
+      messagelog.push({
+        "message": msg.content,
+        "author": msg.author.id
       });
 
-      matched = 1;
+      // Check how many times the same message has been sent.
+      var msgMatch = 0;
+      for (var i = 0; i < messagelog.length; i++) {
+        if (messagelog[i].message == msg.content && (messagelog[i].author == msg.author.id) && (msg.author.id !== bot.user.id)) {
+          msgMatch++;
+        }
+      }
+      // Check matched count
+      if (msgMatch == maxDuplicatesWarning && !warned.includes(msg.author.id)) {
+        warn(msg, msg.author.id);
+      }
+      if (msgMatch == maxDuplicatesBan && !banned.includes(msg.author.id)) {
+        ban(msg, msg.author.id);
+      }
+
+      matched = 0;
 
       for (var i = 0; i < authors.length; i++) {
         if (authors[i].time > now - interval) {
           matched++;
           if (matched == warnBuffer && !warned.includes(msg.author.id)) {
-            warned.push(msg.author.id);
-            msg.channel.send(msg.author + " " + warningMessage);
+            warn(msg, msg.author.id);
           }
-          if (matched == maxBuffer) {
+          else if (matched == maxBuffer) {
             if (!banned.includes(msg.author.id)) {
-              banned.push(msg.author.id);
-              // Ban the user
-              var user = msg.channel.guild.members.find(member => member.user.id === msg.author.id);
-              if (user) {
-                user.ban().then((member) => {
-                  msg.channel.send(msg.author + " " +banMessage);
-               }).catch(() => {
-                  msg.channel.send("insufficient permission to kick " + msg.author + " for spamming.");
-               });
-              }
+              ban(msg, msg.author.id);
             }
           }
         }
         else if (authors[i].time < now - interval) {
-          warned.splice(warned.indexOf(authors[i].author));
-          banned.splice(warned.indexOf(authors[i].author));
           authors.splice(i);
+          warned.splice(warned.indexOf(authors[i]));
+          banned.splice(warned.indexOf(authors[i]));
+        }
+        if (messagelog.length >= 200) {
+          messagelog.shift();
         }
       }
     }
   });
+
+  /**
+   * Warn a user
+   * @param  {Object} msg
+   * @param  {string} userid userid
+   */
+  function warn(msg, userid) {
+    warned.push(msg.author.id);
+    msg.channel.send(msg.author + " " + warningMessage);
+  }
+
+  /**
+   * Ban a user by the user id
+   * @param  {Object} msg
+   * @param  {string} userid userid
+   * @return {boolean} True or False
+   */
+  function ban(msg, userid) {
+    for (var i = 0; i < messagelog.length; i++) {
+      if (messagelog[i].author == msg.author.id) {
+        messagelog.splice(i);
+
+      }
+    }
+
+    banned.push(msg.author.id);
+
+    var user = msg.channel.guild.members.find(member => member.user.id === msg.author.id);
+    if (user) {
+      user.ban().then((member) => {
+        msg.channel.send(msg.author + " " +banMessage);
+        return true;
+     }).catch(() => {
+        msg.channel.send("insufficient permission to kick " + msg.author + " for spamming.");
+        return false;
+     });
+    }
+  }
 
 }
