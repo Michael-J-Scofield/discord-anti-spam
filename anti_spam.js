bf3138d16276a4d63a06e82b2369a9f22f2d2331
Michael <michaelscofield4554@gmail.com>
2017-07-29 13:16:28 +0200

    Optimized warning system

--- anti_spam.js
+++ anti_spam.js
@@ -1,7 +1,6 @@
-var warned = 0;
-var banned = 0;
-
 const authors = [];
+var warned = [];
+var banned = [];
 
 /**
  * Add simple spam protection to your discord server.
@@ -17,8 +16,6 @@ module.exports = function (bot, options) {
   const warningMessage = (options && options.warningMessage) || "stop spamming or I'll whack your head off.";
   const banMessage = (options && options.banMessage) || "has been banned for spamming, anyone else?";
 
-  console.log('Anti spam ready, configuration: \nwarnBuffer: ' + warnBuffer + "\nmaxBuffer: " + maxBuffer + "\nInterval: " + interval);
-
   bot.on('message', msg => {
     if(msg.author.id != bot.user.id){
       var now = Math.floor(Date.now());
@@ -28,35 +25,34 @@ module.exports = function (bot, options) {
         "message": msg.content
       });
 
-      matched = 0;
+      matched = 1;
 
       for (var i = 0; i < authors.length; i++) {
         if (authors[i].time > now - interval) {
           matched++;
-          if (matched == warnBuffer && !warned) {
-            warned = true;
-            msg.reply(warningMessage);
+          if (matched == warnBuffer && !warned.includes(msg.author.id)) {
+            warned.push(msg.author.id);
+            msg.channel.send(msg.author + " " + warningMessage);
           }
           if (matched == maxBuffer) {
-            if (!banned) {
-              msg.channel.send(msg.author + " " +banMessage);
-              banned = true;
-            }
-            // Ban the user
-            var user = msg.channel.guild.members.find(member => member.user.id === msg.author.id);
-            if (user) {
-              user.ban().then((member) => {
-                 console.log("The ban hammer has spoken");
-             }).catch(() => {
-                 //console.log("Unable to ban for spamming, insufficient permissions or user must be a mod");
-             });
+            if (!banned.includes(msg.author.id)) {
+              banned.push(msg.author.id);
+              // Ban the user
+              var user = msg.channel.guild.members.find(member => member.user.id === msg.author.id);
+              if (user) {
+                user.ban().then((member) => {
+                  msg.channel.send(msg.author + " " +banMessage);
+               }).catch(() => {
+                  msg.channel.send("insufficient permission to kick " + msg.author + " for spamming.");
+               });
+              }
             }
           }
         }
         else if (authors[i].time < now - interval) {
+          warned.splice(warned.indexOf(authors[i].author));
+          banned.splice(warned.indexOf(authors[i].author));
           authors.splice(i);
-          warned = false;
-          banned = false;
         }
       }
     }
