e2dd728dfbea8e7c0c4e99502a89282a0b6e6afe
MrAugu <39545629+MrAugu@users.noreply.github.com>
2018-12-09 20:00:29 +0200

    Update anti_spam.js

--- anti_spam.js
+++ anti_spam.js
@@ -1,25 +1,11 @@
+const Discord = require("discord.js");
+
 var authors = [];
 var warned = [];
 var banned = [];
 var messageLog = [];
 
 module.exports = async (client, options) => {
-  /* Defining Functions */
- 
-  // Ban the User by Id
-  const banUser = async (m, user) => {
-    for (var i = 0; i < messageLog.length; i++) {
-      if (messageLog[i].author === m.author.id) {
-        messageLog.splice(i);
-      }
-  } 
-    
-   // Warn the User
-   const warnUser = async (m, reply) => {
-    warned.push(m.author.id);
-    m.channel.send(`<!${m.author.id}>, ${reply}`); // Regular Mention Expression for Mentions
-   }
-  
   /* Option Definitions */
    
   const warnBuffer = (options && options.warnBuffer) || 3; // Default Value: 3
@@ -27,13 +13,42 @@ module.exports = async (client, options) => {
   const interval = (options && options.interval) || 1000; //Default Time: 2000MS (2 Seconds in Miliseconds)
   const warningMessage = (options && options.warningMessage) || "please stop spamming!"; // Default Message: "please stop spamming!" (@User please stop spamming!)
   const banMessage = (options && options.banMessage) || "has been hit by ban hammer for spamming!"; // Default Message: "has been hit by ban hammer for spamming!" (@User has been hit by ban hammer for spamming!)
-  const maxDuplicatesWarning = (options && options. maxDuplicatesWarning || 7); // Default Value: 7
-  const maxDuplicatesBan = (options && options. maxDuplicatesBan || 10); // Deafult Value: 7
+  const maxDuplicatesWarning = (options && options. maxDuplicatesWarning || 7); // Default Value: 7
+  const maxDuplicatesBan = (options && options. maxDuplicatesBan || 10); // Deafult Value: 7
   const deleteMessagesAfterBanForPastDays = (options && options.deleteMessagesAfterBanForPastDays || 7); // Default Value: 10
   const exemptRoles = (options && options.exemptRoles) || []; // Default Value: Nothingness (None)
   const exemptUsers = (options && options.exemptUsers) || []; // Default Value: Nothingness (N  one)
 
- client.on("message", async (message) => {
+ client.on("checkMessage", async (message) => {
+  // Ban the User by Id
+  const banUser = async (m, banMsg) => {
+    for (var i = 0; i < messageLog.length; i++) {
+        if (messageLog[i].author == m.author.id) {
+          messageLog.splice(i);
+        }
+      }
+  
+      banned.push(m.author.id);
+  
+      let user = m.guild.members.get(m.author.id);
+      if (user) {
+        user.ban(deleteMessagesAfterBanForPastDays).then((member) => {
+          m.channel.send(`<@!${m.author.id}>, ${banMsg}`);
+          return true;
+       }).catch(() => {
+          m.channel.send(`Oops, seems like i don't have sufficient permissions to ban <@!${message.author.id}>!`);
+          return false;
+      });
+    }
+  }
+  
+    
+   // Warn the User
+   const warnUser = async (m, reply) => {
+    warned.push(m.author.id);
+    m.channel.send(`<@${m.author.id}>, ${reply}`); // Regular Mention Expression for Mentions
+   }
+
     if (message.author.bot) return;
     if (message.channel.type !== "text" || !message.member || !message.guild || !message.channel.guild) return;
    
@@ -59,10 +74,11 @@ module.exports = async (client, options) => {
       }
       
       if (msgMatch == maxDuplicatesWarning && !warned.includes(message.author.id)) {
-        warn(message, message.author.id);
+        warnUser(message, warningMessage);
       }
+
       if (msgMatch == maxDuplicatesBan && !banned.includes(message.author.id)) {
-        ban(message, message.author.id);
+        banUser(message, banMessage);
       }
 
       var matched = 0;
@@ -71,36 +87,22 @@ module.exports = async (client, options) => {
         if (authors[i].time > currentTime - interval) {
           matched++;
           if (matched == warnBuffer && !warned.includes(message.author.id)) {
-            warn(message, warningMessage);
+            warnUser(message, warningMessage);
           } else if (matched == maxBuffer) {
             if (!banned.includes(message.author.id)) {
-              ban(message, message.author.id);
+              banUser(message, banMessage);
             }
           }
-        } else if (authors[i].time < now - interval) {
+        } else if (authors[i].time < currentTime - interval) {
           authors.splice(i);
           warned.splice(warned.indexOf(authors[i]));
           banned.splice(warned.indexOf(authors[i]));
         }
+
         if (messageLog.length >= 200) {
           messageLog.shift();
         }
       }
     }
   });
-
-    banned.push(message.author.id);
-
-    let user = message.channel.guild.members.get(message.author.id);
-    if (user) {
-      user.(deleteMessagesAfterBanForPastDays).then((member) => {
-        message.channel.send(`<@!${message.author.id}>, ${banMessage}`);
-        return true;
-     }).catch(() => {
-        message.channel.send(`Oops, seems like i don't have sufficient permissions to ban <@!${message.author.id}>!`);
-        return false;
-     });
-    }
-  }
-
 }
