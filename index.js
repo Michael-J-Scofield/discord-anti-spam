85c0f969c6fabc109a78d4d6bcccbe7230840084
MrAugu <mraugu@yahoo.com>
2019-05-31 23:10:00 +0300

    Alot of testing and fixes, now is working fine

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 if (Number(process.version.slice(1).split(".")[0]) < 10) throw new Error("Node 10.0.0 or higher is required. Update Node on your system.");
 
+function falsify(something) {
+  return false;
+}
+
 var users = [];
 var warnedUsers = [];
 var bannedUsers = [];
@@ -8,6 +12,10 @@ const Events = require("events");
 
 class antiSpam extends Events.EventEmitter {
   constructor(options) {
+    super(options);
+
+    if (!options) options = {};
+
     this.warnThreshold = options.warnThreshold || 3;
     this.banThreshold = options.banThreshold || 5;
     this.maxInterval = options.maxInterval || 2000;
@@ -16,44 +24,45 @@ class antiSpam extends Events.EventEmitter {
     this.maxDuplicatesWarning = options.maxDuplicatesWarning || 7;
     this.maxDuplicatesBan = options.maxDuplicatesBan || 10;
     this.deleteMessagesAfterBanForPastDays = options.deleteMessagesAfterBanForPastDays || 1;
-    this.exemptRoles = options.exemptRoles || (role) => return false;
-    this.exemptUsers = options.exemptUsers || (member) => return false;
-    this.exemptGuilds = options.exemptGuilds || (guild) => return false;
+    this.exemptRoles = options.exemptRoles;
+    this.exemptUsers = options.exemptUsers;
+    this.exemptGuilds = options.exemptGuilds;
     this.exemptPermissions = options.exemptPermissions || [];
     this.ignoreBots = options.ignoreBots || true;
     this.verbose = options.verbose || false;
     this.client = options.client;
+    this.extemptRoles = options.extemptRoles || falsify;
+    this.extemptUsers = options.exemptUsers || falsify;
+    this.extemptGuilds = options.extemptGuilds || falsify;
 
     if (!this.client) {
-      this.emit("error", "options.client is not optional.");
+      console.log("[FATAL ERROR]: Discord Anti Spam - options.client is not optional.");
+      process.exit(5);
     }
-
-    this.emit("ready");
   }
 
   message(message) {
     if (this.ignoreBots === true && message.author.bot) return;
     if (message.channel.type !== "text") return;
-    if (!message.default || !message.deleted || !message.channel || !message.author || !message.content) return;
     if (!message.guild || !message.member) return;
     if (this.client && this.client.user && message.author.id === this.client.user.id);
 
     var hasRoleExtempt = false;
     for (const role of message.member.roles) {
       if (hasRoleExtempt === true) return;
-      if (this.extemptRoles(role) && this.extemptRoles(role) === true) {
+      if (this.extemptRoles && this.extemptRoles(role) === true) {
         hasRoleExtempt = true;
         return true;
       }
     }
 
     if (hasRoleExtempt === true) return;
-    if (this.exemptUsers(message.member) && this.exemptUsers(message.member) === true) return;
-    if (this.exemptGuilds(message.guild) && this.exemptGuilds(message.guild) === true) return;
+    if (this.exemptUsers && this.exemptUsers(message.member) === true) return;
+    if (this.exemptGuilds && this.exemptGuilds(message.guild) === true) return;
 
     const banUser = (msg) => {
       for (var i = 0; i < messageCache.length; i++) {
-        if (messageLog[i].author == msg.author.id) {
+        if (messageCache[i].author == msg.author.id) {
           messageCache.splice(i);
         }
       }
@@ -62,7 +71,11 @@ class antiSpam extends Events.EventEmitter {
 
       if (!msg.author.bannable) {
         if (this.verbose == true) console.log(`**${msg.author.tag}** (ID: ${msg.author.id}) could not be banned, insufficient permissions.`);
-        msg.channel.send(`Could not ban **${msg.author.tag}** because of inpropper permissions.`).catch(e => this.verbose === true ? console.log(e));
+        msg.channel.send(`Could not ban **${msg.author.tag}** because of inpropper permissions.`).catch(e => {
+          if (this.verbose === true) {
+            console.log(e);
+          }
+        });
         return false;
       }
 
@@ -71,14 +84,22 @@ class antiSpam extends Events.EventEmitter {
         this.emit("banAdd", msg.member);
       } catch (e) {
         if (this.verbose == true) console.log(`**${msg.author.tag}** (ID: ${msg.author.id}) could not be banned, ${e}.`);
-        msg.channel.send(`Could not ban **${msg.author.tag}** because \`${e}\`.`).catch(e => this.verbose === true ? console.log(e));
+        msg.channel.send(`Could not ban **${msg.author.tag}** because \`${e}\`.`).catch(e => {
+          if (this.verbose === true) {
+            console.log(e);
+          }
+        });
         return false;
       }
 
       var msgToSend = this.banMessage;
       msgToSend = msgToSend.replace(/{user_tag}/g, msg.author.tag);
 
-      msg.channel.send(msgToSend).catch(e => this.verbose === true ? console.log(e));
+      msg.channel.send(msgToSend).catch(e => {
+        if (this.verbose === true) {
+          console.log(e);
+        }
+      });
       return true;
     };
 
@@ -90,7 +111,12 @@ class antiSpam extends Events.EventEmitter {
       msgToSend = msgToSend.replace(/{user_tag}/g, msg.author.tag);
       msgToSend = msgToSend.replace(/{@user}/g, `<@${msg.author.id}>`);
 
-      msg.channel.send(msgToSend).catch(e => this.verbose === true ? console.log(e));
+      msg.channel.send(msgToSend).catch(e => {
+        if (this.verbose === true) {
+          console.log(e);
+        }
+      });
+
       return true;
     };
 
@@ -110,30 +136,30 @@ class antiSpam extends Events.EventEmitter {
       if (messageCache[i].message === message.content && messageCache[i].author === message.author) messageMatches++;
     }
 
-    if (messageMatches === maxDuplicatesWarning && !warnedUsers.includes(message.author.id)) {
+    if (messageMatches === this.maxDuplicatesWarning && !warnedUsers.includes(message.author.id)) {
       warnUser(message);
       this.emit("warnEmit", message.member);
     }
 
-    if (messageMatches === maxDuplicatesBan && !bannedUsers.includes(message.author.id)) {
+    if (messageMatches === this.maxDuplicatesBan && !bannedUsers.includes(message.author.id)) {
       banUser(message);
       this.emit("banEmit", message.member);
     }
 
     var spamMatches = 0;
 
-    for (var i = 0; i < authors.length; i++) {
-      if (authors[i].time > Date.now() - interval) {
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].time > Date.now() - this.maxInterval) {
         spamMatches++;
       }
     }
 
-    if (spamMatches === warnThreshold && !warnedUsers.includes(message.author.id)) {
+    if (spamMatches === this.warnThreshold && !warnedUsers.includes(message.author.id)) {
       warnUser(message);
       this.emit("warnEmit", message.member);
     }
 
-    if (spamMatches === banThreshold && !bannedUsers.includes(message.author.id)) {
+    if (spamMatches === this.banThreshold && !bannedUsers.includes(message.author.id)) {
       banUser(message);
       this.emit("banEmit", message.member);
     }
