891e506656510ee974a4ae10dd7ba2c4c8cbdfc4
NotSugden <leoaustin675@gmail.com>
2019-11-24 21:39:17 +0000

    update formatting

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 if (Number(process.version.split('.')[0].match(/[0-9]+/)) < 10)
-	throw new Error(
-		'Node 10.0.0 or higher is required. Update Node on your system.'
-	);
+	throw new Error('Node 10.0.0 or higher is required. Update Node on your system.');
 const { RichEmbed } = require('discord.js');
 const { EventEmitter } = require('events');
 const defaultOptions = {
@@ -57,13 +55,10 @@ class AntiSpam extends EventEmitter {
 			return false;
 
 		const { options, data } = this;
-		if (!message.member)
-			message.member = await message.guild.fetchMember(message.author);
+		if (!message.member) message.member = await message.guild.fetchMember(message.author);
 		if (
 			(options.ignoreBots && message.author.bot) ||
-			options.exemptPermissions.some(permission =>
-				message.member.hasPermission(permission)
-			)
+			options.exemptPermissions.some(permission => message.member.hasPermission(permission))
 		)
 			return false;
 
@@ -71,8 +66,7 @@ class AntiSpam extends EventEmitter {
 			message.member.roles.some(role =>
 				typeof options.ignoredRoles === 'function'
 					? options.ignoredRoles(role)
-					: options.ignoredRoles.includes(role.id) ||
-					  options.ignoredRoles.includes(role.name)
+					: options.ignoredRoles.includes(role.id) || options.ignoredRoles.includes(role.name)
 			) ||
 			(typeof options.ignoredUsers === 'function'
 				? options.ignoredUsers(message.author)
@@ -87,9 +81,7 @@ class AntiSpam extends EventEmitter {
 			return false;
 
 		const banUser = async () => {
-			data.messageCache = data.messageCache.filter(
-				m => m.author !== message.author.id
-			);
+			data.messageCache = data.messageCache.filter(m => m.author !== message.author.id);
 			data.bannedUsers.push(message.author.id);
 
 			if (!message.member.bannable) {
@@ -106,11 +98,9 @@ class AntiSpam extends EventEmitter {
 					days: options.deleteMessagesAfterBanForPastDays
 				});
 				if (options.banMessage)
-					await message.channel
-						.send(format(options.banMessage, message))
-						.catch(e => {
-							if (options.verbose) console.error(e);
-						});
+					await message.channel.send(format(options.banMessage, message)).catch(e => {
+						if (options.verbose) console.error(e);
+					});
 				this.emit('banAdd', message.member);
 				return true;
 			} catch (error) {
@@ -121,9 +111,7 @@ class AntiSpam extends EventEmitter {
 						`**${message.author.tag}** (ID: ${message.author.id}) could not be banned, ${error}.`
 					);
 				await message.channel
-					.send(
-						`Could not ban **${message.author.tag}** because of an error: \`${error}\`.`
-					)
+					.send(`Could not ban **${message.author.tag}** because of an error: \`${error}\`.`)
 					.catch(e => {
 						if (options.verbose) console.error(e);
 					});
@@ -132,9 +120,7 @@ class AntiSpam extends EventEmitter {
 		};
 
 		const kickUser = async () => {
-			data.messageCache = data.messageCache.filter(
-				m => m.author !== message.author.id
-			);
+			data.messageCache = data.messageCache.filter(m => m.author !== message.author.id);
 			data.kickedUsers.push(message.author.id);
 
 			if (!message.member.kickable) {
@@ -148,11 +134,9 @@ class AntiSpam extends EventEmitter {
 			try {
 				await message.member.kick('Spamming!');
 				if (options.kickMessage)
-					await message.channel
-						.send(format(options.kickMessage, message))
-						.catch(e => {
-							if (options.verbose) console.error(e);
-						});
+					await message.channel.send(format(options.kickMessage, message)).catch(e => {
+						if (options.verbose) console.error(e);
+					});
 				this.emit('kickAdd', message.member);
 				return true;
 			} catch (error) {
@@ -163,9 +147,7 @@ class AntiSpam extends EventEmitter {
 						`**${message.author.tag}** (ID: ${message.author.id}) could not be kicked, ${error}.`
 					);
 				await message.channel
-					.send(
-						`Could not kick **${message.author.tag}** because of an error: \`${error}\`.`
-					)
+					.send(`Could not kick **${message.author.tag}** because of an error: \`${error}\`.`)
 					.catch(e => {
 						if (options.verbose) console.error(e);
 					});
@@ -178,11 +160,9 @@ class AntiSpam extends EventEmitter {
 			this.emit('warnAdd', message.member);
 
 			if (options.warnMessage)
-				await message.channel
-					.send(format(options.warnMessage, message))
-					.catch(e => {
-						if (options.verbose) console.error(e);
-					});
+				await message.channel.send(format(options.warnMessage, message)).catch(e => {
+					if (options.verbose) console.error(e);
+				});
 
 			return true;
 		};
@@ -201,14 +181,12 @@ class AntiSpam extends EventEmitter {
 			m => m.content === message.content && m.author === message.author.id
 		).length;
 		const spamMatches = data.users.filter(
-			u =>
-				u.time > Date.now() - options.maxInterval && u.id === message.author.id
+			u => u.time > Date.now() - options.maxInterval && u.id === message.author.id
 		).length;
 
 		if (
 			!data.warnedUsers.includes(message.author.id) &&
-			(spamMatches === options.warnThreshold ||
-				messageMatches === options.maxDuplicatesWarning)
+			(spamMatches === options.warnThreshold || messageMatches === options.maxDuplicatesWarning)
 		) {
 			if (options.warnEnabled) warnUser(message);
 			this.emit(
@@ -221,28 +199,16 @@ class AntiSpam extends EventEmitter {
 
 		if (
 			!data.kickedUsers.includes(message.author.id) &&
-			(spamMatches === options.kickThreshold ||
-				messageMatches === options.maxDuplicatesKick)
+			(spamMatches === options.kickThreshold || messageMatches === options.maxDuplicatesKick)
 		) {
 			if (options.kickEnabled) await kickUser(message);
-			this.emit(
-				'spamThresholdKick',
-				message.member,
-				messageMatches === options.maxDuplicatesKick
-			);
+			this.emit('spamThresholdKick', message.member, messageMatches === options.maxDuplicatesKick);
 			return true;
 		}
 
-		if (
-			spamMatches === options.banThreshold ||
-			messageMatches === options.maxDuplicatesBan
-		) {
+		if (spamMatches === options.banThreshold || messageMatches === options.maxDuplicatesBan) {
 			if (options.banEnabled) await banUser(message);
-			this.emit(
-				'spamThresholdBan',
-				message.member,
-				messageMatches === options.maxDuplicatesBan
-			);
+			this.emit('spamThresholdBan', message.member, messageMatches === options.maxDuplicatesBan);
 			return true;
 		}
 
@@ -274,12 +240,9 @@ function format(string, message) {
 			.replace(/{user_tag}/g, message.author.tag)
 			.replace(/{server_name}/g, message.guild.name);
 	const embed = new RichEmbed(string);
-	if (embed.description)
-		embed.setDescription(format(embed.description, message));
+	if (embed.description) embed.setDescription(format(embed.description, message));
 	if (embed.title) embed.setTitle(format(title, message));
-	if (embed.footer && embed.footer.text)
-		embed.footer.text = format(embed.footer.text, message);
-	if (embed.author && embed.author.name)
-		embed.author.name = format(embed.author.name, message);
+	if (embed.footer && embed.footer.text) embed.footer.text = format(embed.footer.text, message);
+	if (embed.author && embed.author.name) embed.author.name = format(embed.author.name, message);
 	return embed;
 }
